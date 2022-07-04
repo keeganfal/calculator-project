0ee1d275ec73f1f10fbe60ba397c307f2ddbafb4
keeganfal <60140681+keeganfal@users.noreply.github.com>
2022-07-04 22:27:22 +0100

    js switchcase for numbers

--- script.js
+++ script.js
@@ -1,8 +1,54 @@
 const screen = document.querySelector("#screen");
 const display = document.querySelector(".main__display");
-const buttons = document.querySelectorAll(".main__button")
+const numButtons = document.querySelectorAll(".main__num-button")
+const calcButtons = document.querySelectorAll(".main__calc-button")
 
 currentNum = "";
+
+const handleNumbers = (event) =>{
+    switch (event.target.id) {
+      case "one":
+        console.log("one");
+        display.innerHTML += "1";
+        break;
+      case "two":
+        console.log("two");
+        display.innerHTML += "2";
+        break;
+      case "three":
+        console.log("divide was pressed");
+        display.innerHTML += "3";
+        break;
+      case "four":
+        console.log("four");
+        display.innerHTML += "4";
+        break;
+      case "five":
+        console.log("five");
+        display.innerHTML += "5";
+        break;
+      case "six":
+        console.log("six");
+        display.innerHTML += "6";
+        break;
+      case "seven":
+        console.log("seven");
+        display.innerHTML += "7";
+        break;
+      case "eight":
+        console.log("eight");
+        display.innerHTML += "8";
+        break;
+      case "nine":
+        console.log("nine");
+        display.innerHTML += "9";
+        break;
+      case "zero":
+        console.log("zero");
+        display.innerHTML += "0";
+        break;
+    }
+}
 
 const handleCalculation = (event) =>{
     switch (event.target.id) {
@@ -33,6 +79,10 @@ const handleCalculation = (event) =>{
     }
 }
 
-Array.from(buttons).forEach((button) =>
+Array.from(numButtons).forEach((button) =>
+  button.addEventListener("click", handleNumbers)
+);
+
+Array.from(calcButtons).forEach((button) =>
   button.addEventListener("click", handleCalculation)
 );

1f5e36e1e3d47b55f888141834ec599d43d3c189
keeganfal <60140681+keeganfal@users.noreply.github.com>
2022-07-04 21:35:12 +0100

    js switchcase for calculations

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
 const screen = document.querySelector("#screen");
 const display = document.querySelector(".main__display");
-const acBtn = document.querySelector("#ac-button");
-const plusMinusBtn = document.querySelector("#plus-minus-button");
-const percentBtn = document.querySelector("#percent-button");
-const divideBtn = document.querySelector("#divide-button");
+const buttons = document.querySelectorAll(".main__button")
 
+currentNum = "";
 
-const handleAcClick = (event) =>{
-    console.log("AC button was clicked");
+const handleCalculation = (event) =>{
+    switch (event.target.id) {
+      case "plus-minus-button":
+        console.log("plus minus was pressed");
+        display.innerHTML = "+"
+        break;
+      case "percent-button":
+        console.log("percent was pressed");
+        display.innerHTML = "%"
+        break;
+      case "divide-button":
+        console.log("divide was pressed");
+        display.innerHTML = "÷"
+        break;
+      case "multiply-button":
+        console.log("multiply was pressed");
+        display.innerHTML = "x"
+        break;
+      case "minus-button":
+        console.log("minus was pressed");
+        display.innerHTML = "-"
+        break;
+      case "plus-button":
+        console.log("plus was pressed");
+        display.innerHTML = "+"
+        break;
+    }
 }
 
-const handlePlusMinusClick = (event) =>{
-    console.log("Plus Minus button was clicked");
-}
-
-const handlePercentClick = (event) =>{
-    console.log("Percent button was clicked");
-}
-
-const handleDivideClick = (event) =>{
-    console.log("Divide button was clicked");
-    display.innerHTML = "/"
-}
-
-acBtn.addEventListener("click", handleAcClick)
-plusMinusBtn.addEventListener("click", handlePlusMinusClick)
-percentBtn.addEventListener("click", handlePercentClick)
-divideBtn.addEventListener("click", handleDivideClick)
+Array.from(buttons).forEach((button) =>
+  button.addEventListener("click", handleCalculation)
+);

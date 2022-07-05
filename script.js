f7d265c88a759c122066582ecc9cb5bdbb1a7fa6
keeganfal <60140681+keeganfal@users.noreply.github.com>
2022-07-05 01:42:56 +0100

    js all operators buttons dom created

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
-const screen = document.querySelector("#screen");
-const display = document.querySelector(".main__display");
+const displayHistory = document.querySelector(".main__display-history");
+const displayOutput = document.querySelector(".main__display-output");
 const numButtons = document.querySelectorAll(".main__num-button")
-const calcButtons = document.querySelectorAll(".main__calc-button")
+const calcButtons = document.querySelectorAll(".main__operator-button")
 
 currentNum = "";
 
+const getOutput = () => { 
+    return displayOutput.innerHTML;
+};
 
-const handleNumbers = (event) =>{
+const printOutput = (num) => { 
+    return displayOutput.innerHTML = num;
+};
+
+
+const handleNumbersClick = (event) =>{
     switch (event.target.id) {
       case "one":
         console.log("one");
-        display.innerHTML += "1";
+        displayOutput.innerHTML += "1";
         break;
       case "two":
         console.log("two");
-        display.innerHTML += "2";
+        displayOutput.innerHTML += "2";
         break;
       case "three":
-        console.log("divide was pressed");
-        display.innerHTML += "3";
+        console.log("3");
+        displayOutput.innerHTML += "3";
         break;
       case "four":
         console.log("four");
@@ -26,64 +34,61 @@ const handleNumbers = (event) =>{
         break;
       case "five":
         console.log("five");
-        display.innerHTML += "5";
+        displayOutput.innerHTML += "5";
         break;
       case "six":
         console.log("six");
-        display.innerHTML += "6";
+        displayOutput.innerHTML += "6";
         break;
       case "seven":
         console.log("seven");
-        display.innerHTML += "7";
+        displayOutput.innerHTML += "7";
         break;
       case "eight":
         console.log("eight");
-        display.innerHTML += "8";
+        displayOutput.innerHTML += "8";
         break;
       case "nine":
         console.log("nine");
-        display.innerHTML += "9";
+        displayOutput.innerHTML += "9";
         break;
       case "zero":
         console.log("zero");
-        display.innerHTML += "0";
+        displayOutput.innerHTML += "0";
         break;
     }
 }
 
-const handleCalculation = (event) =>{
+const handleOperatorClick = (event) =>{
     switch (event.target.id) {
-      case "plus-minus-button":
-        console.log("plus minus was pressed");
-        display.innerHTML = "+"
-        break;
-      case "percent-button":
-        console.log("percent was pressed");
-        display.innerHTML = "%"
-        break;
-      case "divide-button":
-        console.log("divide was pressed");
-        display.innerHTML = "÷"
-        break;
-      case "multiply-button":
-        console.log("multiply was pressed");
-        display.innerHTML = "x"
-        break;
-      case "minus-button":
-        console.log("minus was pressed");
-        display.innerHTML = "-"
-        break;
-      case "plus-button":
-        console.log("plus was pressed");
-        display.innerHTML = "+"
-        break;
+        case "ac-button":
+            console.log("ac was pressed");
+            displayOutput.innerHTML = ""
+            displayHistory.innerHTML = ""
+            break;
+        case "divide-button":
+            console.log("divide was pressed");
+            displayOutput.innerHTML = "÷"
+            break;
+        case "multiply-button":
+            console.log("multiply was pressed");
+            displayOutput.innerHTML = "x"
+            break;
+        case "minus-button":
+            console.log("minus was pressed");
+            displayOutput.innerHTML = "-"
+            break;
+        case "plus-button":
+            console.log("plus was pressed");
+            displayOutput.innerHTML = "+"
+            break;
     }
 }
 
 Array.from(numButtons).forEach((button) =>
-  button.addEventListener("click", handleNumbers)
+  button.addEventListener("click", handleNumbersClick)
 );
 
 Array.from(calcButtons).forEach((button) =>
-  button.addEventListener("click", handleCalculation)
+  button.addEventListener("click", handleOperatorClick)
 );
